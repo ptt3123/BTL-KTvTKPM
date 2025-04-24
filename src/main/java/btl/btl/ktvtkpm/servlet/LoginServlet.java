723bd4a8b40ccce1108f9219d28f54081c87e45e
Phan Tien Tai <taip7547@gmail.com>
2025-04-24 09:43:38 +0700

    Login and Register

---
+++ src/main/java/btl/btl/ktvtkpm/servlet/LoginServlet.java
@@ -0,0 +1,74 @@
+package btl.btl.ktvtkpm.servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import btl.btl.ktvtkpm.dao.UserDAO;
+import btl.btl.ktvtkpm.model.User;
+import btl.btl.ktvtkpm.util.PasswordUtil;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("register.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        
+        // Lấy dữ liệu từ form đăng nhập
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Kiểm tra dữ liệu không rỗng
+        if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+            request.setAttribute("errorMessage", "Tên đăng nhập và mật khẩu không được để trống!");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            return;
+        }
+        
+        // Check Login
+        try (UserDAO userDAO = new UserDAO();) {
+            User user = userDAO.readByUsername(username);
+            
+            if((user == null) || (!PasswordUtil.checkPassword(password, user.getPassword()))){
+                request.setAttribute("errorMessage", "Thông tin đăng nhập không chính xác!");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+                
+            } else {
+                // Lưu thông tin 
+                HttpSession session = request.getSession();
+                session.setAttribute("uid", user.getId());
+                boolean ist = user.isIsStaff();
+                session.setAttribute("ist", ist);
+
+                // Chuyển hướng đến trang chủ hoặc trang dashboard
+                if (!ist) {
+                    response.sendRedirect("index.jsp");
+                } else {
+                    response.sendRedirect("dashboard.jsp");
+                }
+            }
+            
+        } catch (Exception e) {
+            request.setAttribute("errorMessage", "Đã có lỗi xảy ra. Hãy thử lại sau!");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/btl/btl/ktvtkpm/servlet/RegisterServlet.java
@@ -0,0 +1,120 @@
+package btl.btl.ktvtkpm.servlet;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.Part;
+import java.sql.Date;
+import java.util.UUID;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import java.util.Set;
+import btl.btl.ktvtkpm.model.User;
+import btl.btl.ktvtkpm.dao.UserDAO;
+import btl.btl.ktvtkpm.util.FileUploadUtil;
+import java.sql.SQLIntegrityConstraintViolationException;
+import btl.btl.ktvtkpm.util.UploadPath;
+
+@WebServlet("/register")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+                 maxFileSize = 1024 * 1024 * 10, // 10MB
+                 maxRequestSize = 1024 * 1024 * 50) // 50MB
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("register.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        
+        // Lấy dữ liệu
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+        String familyName = request.getParameter("familyName");
+        String phoneNumber = request.getParameter("phoneNumber");
+        String address = request.getParameter("address");
+        String dateOfBirthStr = request.getParameter("dateOfBirth");
+        Part filePart = request.getPart("avatar");
+        
+        // Convert ngày sinh
+        Date sqlDate = Date.valueOf(dateOfBirthStr);
+        
+        // Tạo link cho avatar
+        String avatarPath = "0.jpg";
+        if(filePart != null && filePart.getSize() > 0){
+            
+            // Lấy phần mở rộng của file
+            String fileExtension = FileUploadUtil.getFileExtension(filePart);
+            if (!FileUploadUtil.isValidFileType(fileExtension)) {
+                request.setAttribute("errorMessage", "Định dạng file không hợp lệ!");
+                request.getRequestDispatcher("register.jsp").forward(request, response);
+            }
+            
+            avatarPath = UUID.randomUUID().toString() + fileExtension;
+        }
+
+        // Tạo đối tượng User
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setName(name);
+        user.setFamilyName(familyName);
+        user.setPhoneNumber(phoneNumber);
+        user.setAddress(address);
+        user.setAvatar(avatarPath);
+        user.setDateOfBirth(sqlDate);
+        
+        // Kiểm tra Validation
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+
+        if (!violations.isEmpty()) {
+            StringBuilder errorMessage = new StringBuilder("Lỗi:<br>");
+            for (ConstraintViolation<User> violation : violations) {
+                errorMessage.append("- ").append(violation.getMessage()).append("<br>");
+            }
+            request.setAttribute("errorMessage", errorMessage.toString());
+            request.getRequestDispatcher("register.jsp").forward(request, response);
+            return;
+        }
+        
+        // Lưu vào database
+        try (UserDAO userDAO = new UserDAO();) {
+            if(userDAO.create(user)){
+                if(!"0.jpg".equals(avatarPath)){
+                    FileUploadUtil.saveFile(filePart, avatarPath, UploadPath.USER_AVT_DIR.getPath());
+                }
+                
+                response.sendRedirect("login.jsp"); // Chuyển đến trang đăng nhập
+                
+            } 
+            
+        } catch (SQLIntegrityConstraintViolationException e) {
+            request.setAttribute("errorMessage", e.getMessage());
+            request.getRequestDispatcher("register.jsp").forward(request, response);
+            
+        } catch (Exception e){
+            request.setAttribute("errorMessage", "Đăng ký thất bại, thử lại!");
+            request.getRequestDispatcher("register.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
